52680a22e30019ab1107c0bb935c1a58c3485781
GennaroBiondi <throwawayishere@ultrakill.com>
2026-06-04 13:45:37 +0200

    test(benchmarks): Fix tests using always 10k entries

--- src/bin/gen_data.rs
+++ src/bin/gen_data.rs
@@ -3,7 +3,7 @@ use std::io::Write;
 use std::path::{Path, PathBuf};
 use std::time::{SystemTime, UNIX_EPOCH};
 
-fn gen_file(path: &Path) {
+fn gen_file(path: &Path, entries: usize) {
     println!("Generating content for file at path: {}", path.display());
 
     let words = [
@@ -21,9 +21,9 @@ fn gen_file(path: &Path) {
         .unwrap()
         .as_secs() as i64;
 
-    for i in 0..10_000usize {
+    for i in 0..entries {
         let entry = words[i % words.len()];
-        let ts = now - (10_000 - i) as i64 * 60;
+        let ts = now - (entries - i) as i64 * 60;
         writeln!(file, "{} {}|{}", entry, i, ts).unwrap();
     }
 }
@@ -33,6 +33,6 @@ fn main() {
     let test_10k = PathBuf::from("benches/testing_data/file_10k.test");
     let test_100k = PathBuf::from("benches/testing_data/file_100k.test");
 
-    gen_file(&test_10k);
-    gen_file(&test_100k);
+    gen_file(&test_10k, 10_000);
+    gen_file(&test_100k, 100_000);
 }
